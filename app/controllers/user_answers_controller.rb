f80fc939f1881973e1d5c3a5fbe6fb296306d48e
Ryan Kam <rkam0725@gmail.com>
2021-06-01 15:45:42 +0900

    created update function for user_answers: status and action_step

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,16 +1,25 @@
 class UserAnswersController < ApplicationController
+  # before_action :set_user_answer, only: [:update]
+
 
   def index
-    # @user_answers = policy_scope(UserAnswer).all
-    @user_answers = UserAnswer.all
+    @user_answers = policy_scope(UserAnswer).all.order(created_at: :asc)
+    # @user_answers = UserAnswer.all
+  end
+
+  def update
+    @user_answer = UserAnswer.find(params[:id])
+    authorize @user_answer
+    @user_answer.update(user_answer_params)
+    redirect_to user_answers_path
   end
 
   private
 
-  # def set_user_answer
-  #   @user_answer = UserAnswer.find(params[:id])
-  #   authorize @user_answers
-  # end
+  def set_user_answer
+    @user_answer = UserAnswer.find(params[:id])
+    authorize @user_answers
+  end
 
   def user_answer_params
     params.require(:user_answer).permit(:category, :status, :content, :action_step)
