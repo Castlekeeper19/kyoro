4dbd494933c4233522777dd7a758ace56bc97e95
Luke Chatelain <jens.chatelain@gmail.com>
2021-06-01 12:58:33 +0900

    generated dashboard controller and chart kick

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+  has_many :user_answers
 end
