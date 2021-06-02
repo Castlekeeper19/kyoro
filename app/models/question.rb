ca345c4c7fffd9ebab019b322eed2190676639fc
Ryan Kam <rkam0725@gmail.com>
2021-06-02 14:35:55 +0900

    survey create form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :survey
   has_many :answers
+  accepts_nested_attributes_for :answers
+
   has_many :user_answers, through: :answers
 
   validates :content, presence: true
