87ffd8539e9bbec138ea961b56ad3b4f23c529bc
Luke Chatelain <jens.chatelain@gmail.com>
2021-06-02 14:21:06 +0900

    added feeling calculator method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
   has_many :user_answers
+
+  def overall(category)
+    feeling = 0
+    count = 0
+    self.user_answers.each do |user_answer|
+      if user_answer.answer.question.category == category
+        feeling += user_answer.content.to_i
+        count += 1
+    end
+  end
+   feeling > 0 ? (feeling.to_f/count) : feeling
+ end
 end
