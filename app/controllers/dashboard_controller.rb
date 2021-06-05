55544b27d58833f6aadf0eb586820eeceb09a8fe
Luke Chatelain <jens.chatelain@gmail.com>
2021-06-05 14:50:16 +0900

    dashboards colors change & reformated dashboard index/partials

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,11 @@ class DashboardController < ApplicationController
   def index
     @users = policy_scope(User)
     @survey = policy_scope(Survey).order(:date_sent).last
-    @surveys = policy_scope(Survey).all
-    @user_answers = policy_scope(UserAnswer).all
-    @team_mood = policy_scope(UserAnswer).overall("mood")
-    @team_support = policy_scope(UserAnswer).overall("support")
-    @team_motivation = policy_scope(UserAnswer).overall("motivation")
-    @team_goals = policy_scope(UserAnswer).overall("personal_goals")
+    @user_answers = policy_scope(UserAnswer).includes(:answer)
+    @team_mood = @user_answers.overall("mood")
+    @team_support = @user_answers.overall("support")
+    @team_motivation = @user_answers.overall("motivation")
+    @team_goals = @user_answers.overall("personal_goals")
     @action_steps = policy_scope(UserAnswer).where.not(action_step: [nil, ''], status: "resolved")
   end
 end
