e1bcb1d112a8c2b2c37aaa8aad8b529a34831c82
Luke Chatelain <jens.chatelain@gmail.com>
2021-06-05 12:31:23 +0900

    added admin gem/feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_025514) do
+ActiveRecord::Schema.define(version: 2021_06_05_032236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_025514) do
     t.text "goal"
     t.text "note"
     t.string "nature", default: [], array: true
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
