2f4f77829a788f6cf2457452a6f62e1134632ada
Luke Chatelain <jens.chatelain@gmail.com>
2021-06-07 19:53:40 +0900

    added sidekiq and send survey job

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :surveys, only: [:index, :show, :new, :create ]
---
+++ test/jobs/send_survey_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SendSurveyJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
