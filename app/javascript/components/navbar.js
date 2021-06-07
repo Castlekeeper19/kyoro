2ff4ca507bafc91513922134568c0473a7f429ff
Emilia Zysk <emzysk.dev@gmail.com>
2021-06-07 17:54:16 +0900

    home page partially done

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,11 @@
 
 const navbarScroll = () => {
   const navbar = document.querySelector('.navbar');
-  console.log(navbar);
+  // console.log(navbar);
   // const showBgc = document.querySelector('.navbar-collapse');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      console.log(window.scrollY);
+      // console.log(window.scrollY);
       if (window.scrollY >= 5) {
         navbar.classList.add('navbar-white');
       } else {
